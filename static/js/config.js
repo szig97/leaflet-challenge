14b9127b68f12ea53329cb8481f582c247076975
Sam Ziegler <szig97@outlook.com>
2021-05-03 21:01:24 -0500

    adding earthquakes url and creating earthquake layer groups

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1Ijoic3ppZzk3IiwiYSI6ImNrbnRpb3M5NDAwODQydnA0dnd6N2xwcGMifQ.wdPOj2esFOyT0fyvsHXg8Q";
---
+++ static/js/logic.js
@@ -0,0 +1,6 @@
+// GeoJSON URL Variables
+var earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_hour.geojson"
+
+// Initialize and Create LayerGroups
+var earthquakes = new L.LayerGroup();
+
