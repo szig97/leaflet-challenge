6f15f98cfa97f230bb9f31761c9f4b8f0aceb498
Sam Ziegler <szig97@outlook.com>
2021-05-03 21:11:18 -0500

    defining variables for tile layers (earthquakes)

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,3 +4,12 @@ var earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/
 // Initialize and Create LayerGroups
 var earthquakes = new L.LayerGroup();
 
+// Define Variables for Tile Layers
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
+});
